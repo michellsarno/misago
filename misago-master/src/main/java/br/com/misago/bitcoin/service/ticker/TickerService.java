29e96e1aebab166bbf995bbe26d482959c1b0498
Michell Matheus Sarno Silva <michellsarno@gmail.com>
2018-09-17 07:31:54 -0300

    arrumando layout

--- misago-master/src/main/java/br/com/misago/bitcoin/service/ticker/TickerService.java
+++ misago-master/src/main/java/br/com/misago/bitcoin/service/ticker/TickerService.java
@@ -14,19 +14,19 @@ public class TickerService {
 		
 		List<TickerVo> listTicker = new ArrayList<TickerVo>();
 		
-		TickerVo tickerVo = new TickerVo();
-		
-		tickerVo.setLocate("CN");
-		tickerVo.setExchange("Exemplo");
-		tickerVo.setBuy("100,00");
-		tickerVo.setSell("110,00");
-		tickerVo.setHigh("120,00");
-		tickerVo.setLow("90,00");
-		tickerVo.setVol("500,00");
-		tickerVo.setLast("115,00");
-		tickerVo.setDate("2018-08-12");
-		
-		listTicker.add(tickerVo);
+//		TickerVo tickerVo = new TickerVo();
+//		
+//		tickerVo.setLocate("CN");
+//		tickerVo.setExchange("Exemplo");
+//		tickerVo.setBuy("100,00");
+//		tickerVo.setSell("110,00");
+//		tickerVo.setHigh("120,00");
+//		tickerVo.setLow("90,00");
+//		tickerVo.setVol("500,00");
+//		tickerVo.setLast("115,00");
+//		tickerVo.setDate("2018-08-12");
+//		
+//		listTicker.add(tickerVo);
 		
 		
 		listTicker.add( new TickerBRMercadoBitCoin("BTC").getTicker() );
